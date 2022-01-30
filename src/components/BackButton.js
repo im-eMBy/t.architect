b2eb844a7d51b7133009e9450551d159a2f1581c
Michał <micbie981@gmail.com>
2022-01-30 18:52:22 +0100

    working on mobile nav

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,6 +1,20 @@
 import { useHistory } from "react-router-dom";
 
+import backIcon from "../images/back-icon.png";
+
+import "../scss/_back-button.scss";
+
 export const BackButton = () => {
     const history = useHistory();
-    return <button className="back-button" onClick={history.goBack}><i className="fas fa-chevron-circle-left" />&nbsp;BACK</button>
+    return <button className="back-button" onClick={() => history.goBack()}>
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            width="337.073px" height="337.073px" viewBox="0 0 337.073 337.073">
+            <path d="M168.537,0.006C75.606,0.006,0,75.612,0,168.537c0,92.93,75.606,168.53,168.537,168.53
+		    c92.924,0,168.537-75.601,168.537-168.53C337.073,75.612,261.473,0.006,168.537,0.006z M224.327,253.961
+		    c4.816,4.815,4.816,12.622,0,17.438c-2.408,2.402-5.561,3.615-8.719,3.615c-3.164,0-6.305-1.213-8.719-3.615l-94.144-94.131
+		    c-4.816-4.828-4.816-12.637,0-17.453l94.144-94.134c4.815-4.819,12.622-4.819,17.438,0c4.816,4.815,4.816,12.619,0,17.426
+		    l-85.424,85.431L224.327,253.961z"/>
+        </svg>
+        <span>BACK</span>
+    </button>
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,39 @@
 import { NavLink, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export const Navigation = ({logoUrl}) => {
+export const Navigation = ({ logoUrl }) => {
+    const [hamburgerMenu, setHamburgerMenu] = useState(window.matchMedia("(max-width: 800px)").matches);
+    const [menuOpen, setMenuOpen] = useState(false);
+    useEffect(() => {
+        const windowWatcher = window.matchMedia("(max-width: 800px)");
+        const change = (ev) => setHamburgerMenu(ev.matches);
+        windowWatcher.addEventListener("change", change);
+        return () => {
+            windowWatcher.removeEventListener("change", change);
+        }
+    }, [])
 
     return <nav className="navigation">
-        <Link className="navigation__logo-link" to="/"><img className="navigation__logo" src={`${logoUrl}`} alt="Navigation logo"/></Link>
-        <ul className="navigation__list">
-            <li className="navigation__element"><NavLink to="/projects">projects</NavLink></li>
-            <li className="navigation__element"><NavLink to="/interiors">interiors</NavLink></li>
-            <li className="navigation__element"><NavLink to="/awards">awards</NavLink></li>
-            <li className="navigation__element"><NavLink to="/about">about&nbsp;me</NavLink></li>
-        </ul>
+        <Link className="navigation__logo-link" to="/"><img className="navigation__logo" src={`${logoUrl}`} alt="Navigation logo" /></Link>
+        {hamburgerMenu ?
+            <>
+                <button className="navigation__hamburger-button" onClick={() => setMenuOpen(!menuOpen)}>
+                    hamburger
+                </button>
+                <ul className="navigation__mobile-list" style={menuOpen ? {left:"0", visibility:"visible"} : {left:"100%", visibility:"hidden"}}>
+                    <li className="navigation__element"><NavLink to="/projects">projects</NavLink></li>
+                    <li className="navigation__element"><NavLink to="/interiors">interiors</NavLink></li>
+                    <li className="navigation__element"><NavLink to="/awards">awards</NavLink></li>
+                    <li className="navigation__element"><NavLink to="/about">about&nbsp;me</NavLink></li>
+                </ul>
+            </>
+            :
+            <ul className="navigation__list">
+                <li className="navigation__element"><NavLink to="/projects">projects</NavLink></li>
+                <li className="navigation__element"><NavLink to="/interiors">interiors</NavLink></li>
+                <li className="navigation__element"><NavLink to="/awards">awards</NavLink></li>
+                <li className="navigation__element"><NavLink to="/about">about&nbsp;me</NavLink></li>
+            </ul>
+        }
     </nav>
 }

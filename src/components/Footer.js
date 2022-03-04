48d9bb96d157f1c79455a7b8cc4fcd25a0b0fe54
Michał <micbie981@gmail.com>
2022-03-04 14:45:56 +0100

    hovers on mobile removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 export const Footer = () => {
     return <footer className='footer'>
-        <p>all rights reserved || 2021</p>
+        <p>all rights reserved || 2022</p>
     </footer>
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,11 @@
 import { NavLink, Link } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 
-import navLogo from "../images/nav-logo.png";
-
 export const Navigation = () => {
     const [hamburgerMenu, setHamburgerMenu] = useState(window.matchMedia("(max-width: 800px)").matches);
     const [menuOpen, setMenuOpen] = useState(false);
     const hamburgerElement = useRef(null);
+
     useEffect(() => {
         const windowWatcher = window.matchMedia("(max-width: 800px)");
         const change = (ev) => setHamburgerMenu(ev.matches);
@@ -21,31 +20,42 @@ export const Navigation = () => {
         hamburgerElement.current.classList.toggle("navigation__hamburger-button--open");
     }
 
-    return <nav className="navigation">
-        <Link className="navigation__logo-link" to="/"><img className="navigation__logo" src={navLogo} alt="Navigation logo" /></Link>
-        {hamburgerMenu ?
-            <>
-                <button ref={hamburgerElement} className="navigation__hamburger-button" onClick={() => handleHamburgerClick()}>
-                    <svg viewBox="0 0 18 15" width="24" height="24">
-                        <path className="hamburger--top" fill="#000000" d="M18,1.484c0,0.82-0.665,1.484-1.484,1.484H1.484C0.665,2.969,0,2.304,0,1.484l0,0C0,0.665,0.665,0,1.484,0 h15.031C17.335,0,18,0.665,18,1.484L18,1.484z" />
-                        <path className="hamburger--middle" fill="#000000" d="M18,7.516C18,8.335,17.335,9,16.516,9H1.484C0.665,9,0,8.335,0,7.516l0,0c0-0.82,0.665-1.484,1.484-1.484 h15.031C17.335,6.031,18,6.696,18,7.516L18,7.516z" />
-                        <path className="hamburger--bottom" fill="#000000" d="M18,13.516C18,14.335,17.335,15,16.516,15H1.484C0.665,15,0,14.335,0,13.516l0,0 c0-0.82,0.665-1.484,1.484-1.484h15.031C17.335,12.031,18,12.696,18,13.516L18,13.516z" />
-                    </svg>
-                </button>
-                <ul className="navigation__mobile-list" style={menuOpen ? { left: "0", visibility: "visible" } : { left: "100%", visibility: "hidden" }}>
-                    <li className="navigation__element"><NavLink to="/projects" onClick={() => handleHamburgerClick()}>projects</NavLink></li>
-                    <li className="navigation__element"><NavLink to="/interiors" onClick={() => handleHamburgerClick()}>interiors</NavLink></li>
-                    <li className="navigation__element"><NavLink to="/awards" onClick={() => handleHamburgerClick()}>awards</NavLink></li>
-                    <li className="navigation__element"><NavLink to="/about" onClick={() => handleHamburgerClick()}>about&nbsp;me</NavLink></li>
-                </ul>
-            </>
-            :
-            <ul className="navigation__list">
-                <li className="navigation__element"><NavLink to="/projects">projects</NavLink></li>
-                <li className="navigation__element"><NavLink to="/interiors">interiors</NavLink></li>
-                <li className="navigation__element"><NavLink to="/awards">awards</NavLink></li>
-                <li className="navigation__element"><NavLink to="/about">about&nbsp;me</NavLink></li>
+    const getMobileNav = () => {
+        return <>
+            <button ref={hamburgerElement} className="navigation__hamburger-button" onClick={handleHamburgerClick}>
+                <svg viewBox="0 0 18 15" width="24" height="24">
+                    <path className="hamburger--top" fill="#000000" d="M18,1.484c0,0.82-0.665,1.484-1.484,1.484H1.484C0.665,2.969,0,2.304,0,1.484l0,0C0,0.665,0.665,0,1.484,0 h15.031C17.335,0,18,0.665,18,1.484L18,1.484z" />
+                    <path className="hamburger--middle" fill="#000000" d="M18,7.516C18,8.335,17.335,9,16.516,9H1.484C0.665,9,0,8.335,0,7.516l0,0c0-0.82,0.665-1.484,1.484-1.484 h15.031C17.335,6.031,18,6.696,18,7.516L18,7.516z" />
+                    <path className="hamburger--bottom" fill="#000000" d="M18,13.516C18,14.335,17.335,15,16.516,15H1.484C0.665,15,0,14.335,0,13.516l0,0 c0-0.82,0.665-1.484,1.484-1.484h15.031C17.335,12.031,18,12.696,18,13.516L18,13.516z" />
+                </svg>
+            </button>
+            <ul className="navigation__mobile-list" style={menuOpen ? { left: "0", visibility: "visible" } : { left: "100%", visibility: "hidden" }}>
+                <li className="navigation__element"><NavLink to="/projects" onClick={handleHamburgerClick}>projects</NavLink></li>
+                <li className="navigation__element"><NavLink to="/interiors" onClick={handleHamburgerClick}>interiors</NavLink></li>
+                <li className="navigation__element"><NavLink to="/awards" onClick={handleHamburgerClick}>awards</NavLink></li>
+                <li className="navigation__element"><NavLink to="/about" onClick={handleHamburgerClick}>about&nbsp;me</NavLink></li>
             </ul>
-        }
+        </>
+    }
+    const getNav = () => {
+        return <ul className="navigation__list">
+            <li className="navigation__element"><NavLink to="/projects">projects</NavLink></li>
+            <li className="navigation__element"><NavLink to="/interiors">interiors</NavLink></li>
+            <li className="navigation__element"><NavLink to="/awards">awards</NavLink></li>
+            <li className="navigation__element"><NavLink to="/about">about&nbsp;me</NavLink></li>
+        </ul>
+    }
+
+    return <nav className="navigation">
+        <Link className="navigation__logo" to="/">
+            <svg width="169" height="79" viewBox="0 0 169 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="40" height="40" fill="#050708" />
+                <rect x="129" y="39" width="40" height="40" fill="#050708" />
+                <rect x="78" width="40" height="40" fill="#050708" />
+                <rect x="39" y="39" width="40" height="40" fill="#050708" />
+                <rect x="39" width="40" height="40" fill="#050708" />
+            </svg>
+        </Link>
+        {hamburgerMenu ? getMobileNav() : getNav()}
     </nav>
 }
